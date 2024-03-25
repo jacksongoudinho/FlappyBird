2ea38dd860f4c3e9433b1f010f8cd240362f3458
Jackson Goudinho <jacksongjunior@gmail.com>
2024-03-25 20:05:26 -0300

    Criação do GameManager

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -6,7 +6,6 @@ public class Bird : MonoBehaviour
 {
     public Rigidbody2D rig;
     public float jumpForce;
-    public BirdStatus status = BirdStatus.Start;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +16,15 @@ public class Bird : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (status)
+        switch (GameManager.instance.status)
         {
-            case BirdStatus.Start:
+            case GameStatus.Start:
                 StartUpdate();
                     break;
-            case BirdStatus.Play:
+            case GameStatus.Play:
                 PlayUpdate();
                     break;
-            case BirdStatus.GameOver:
+            case GameStatus.GameOver:
                 GameOverUpdate();
                     break;
         }
@@ -36,7 +35,7 @@ public class Bird : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            status = BirdStatus.Play;
+            GameManager.instance.StartGame();
             rig.bodyType = RigidbodyType2D.Dynamic;
             Jump();
         }
@@ -59,20 +58,13 @@ public class Bird : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        status = BirdStatus.GameOver;
+        GameManager.instance.GameOver();
     }
 
 
     void GameOverUpdate()
     {
     
-    }
-
-    public enum BirdStatus
-    {
-        Start,
-        Play,
-        GameOver
     }
 }
 
